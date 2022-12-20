89747750b286026daa202a34885fdd04ed0f35f4
mikakidman <kitmanovamz@gmail.com>
2022-12-19 19:42:22 -0500

    v2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        SolarSystem Jinny=new SolarSystem();
+        Jinny.Sun=1;
+        Jinny.Moon=2;
+        Jinny.Planets=3;
     }
 }
